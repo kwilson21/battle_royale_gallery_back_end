1338f46dfd8b57eaf918cce91ca43fe726cea831
Kazon Wilson <kazonwilson@gmail.com>
2018-10-06 19:03:51 -0700

    Fixed CORS issue

--- startup/routes.js
+++ startup/routes.js
@@ -14,14 +14,6 @@ const auth = require("../routes/auth");
 module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: false }, { limit: "16mb" }));
   app.use(bodyParser.json({ limit: "16mb" }));
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   app.use("/api/games", games);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
