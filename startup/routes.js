e2e3073ae7cd91fbc86b4a80b8d740281da78bae
Kazon Wilson <kazonwilson@gmail.com>
2018-10-06 19:24:22 -0700

    Fixed CORS issue

--- startup/routes.js
+++ startup/routes.js
@@ -6,31 +6,35 @@ const morgan = require("morgan");
 // Middleware
 const error = require("../middleware/error");
 
-
 // Routes
 const games = require("../routes/games");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 
 module.exports = function(app) {
-  app.use(function (req, res, next) {
-
+  app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://protected-river-46571.herokuapp.com/');
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
     // Pass to next layer of middleware
     next();
-});
+  });
   app.use(bodyParser.urlencoded({ extended: false }, { limit: "16mb" }));
   app.use(bodyParser.json({ limit: "16mb" }));
   app.use("/api/games", games);
